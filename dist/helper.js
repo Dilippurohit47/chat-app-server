3598d2b80967c1f21823fd1557118ee7ddeb2b18
Dilippurohit47 <dilippurohit204@gmail.com>
2026-03-27 00:45:26 +0530

    delete the old dist folder

--- dist/helper.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logWarn = void 0;
-const logWarn = (error) => {
-    // console.log(error)
-};
-exports.logWarn = logWarn;
